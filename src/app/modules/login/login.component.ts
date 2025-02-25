c50e6cc5bb57817a777f8d241e6c9238369fd2f3
christian renovell miralles <57290453+ChristianRenovell@users.noreply.github.com>
2025-02-25 19:17:31 +0000

    login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { environment } from '../../environment/environment';
 import {
   createUserWithEmailAndPassword,
@@ -8,7 +14,7 @@ import {
 } from 'firebase/auth';
 import { GoogleAuthProvider } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import {  LoginService } from '../../services/login.service';
+import { LoginService } from '../../services/login.service';
 import { LoginResponse } from '../../user.interface';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ButtonModule } from 'primeng/button';
@@ -16,20 +22,40 @@ import { InputTextModule } from 'primeng/inputtext';
 import { CardModule } from 'primeng/card';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CheckboxModule, ButtonModule, InputTextModule, CardModule, FloatLabelModule],
+  imports: [
+    CheckboxModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    FloatLabelModule,
+    ReactiveFormsModule,
+  ],
   styleUrl: './login.component.scss',
   templateUrl: './login.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class LoginComponent implements OnInit {
-
-
-  private _loginServices = inject(LoginService)
+  private _loginServices = inject(LoginService);
   private readonly _router = inject(Router);
+  private readonly _formBuilder = inject(FormBuilder);
+
+  protected form: FormGroup = this._formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    check: [false as boolean, [Validators.required]],
+  });
+
+  isRegistre = signal<boolean>(false)
 
   ngOnInit(): void {
     initializeApp(environment.firebaseConfig);
@@ -40,15 +66,15 @@ export default class LoginComponent implements OnInit {
     const provider = new GoogleAuthProvider();
     try {
       const result = await signInWithPopup(auth, provider);
-    
+
       const idToken = await result.user.getIdToken();
 
       this._loginServices.login(idToken).subscribe((result: LoginResponse) => {
-        localStorage.setItem("token-recipes", result.token);
+        localStorage.setItem('token-recipes', result.token);
       });
-      this._goToSearch()
+      this._goToSearch();
     } catch (error) {
-      console.error("Error en loginWithGoogle:", error);
+      console.error('Error en loginWithGoogle:', error);
     }
   }
 
@@ -57,32 +83,47 @@ export default class LoginComponent implements OnInit {
   }
 
   async loginWithEmailAndPassword() {
-    const auth = getAuth();
-    try {
-      const result = await signInWithEmailAndPassword(auth, "christianrenovell83@gmail.com", "123456Abc");
-      const idToken = await result.user.getIdToken();
-      console.log(idToken);
-      this._loginServices.login(idToken).subscribe((result: LoginResponse) => {
-        localStorage.setItem("token-recipes", result.token);
-      });
-      this._goToSearch()
-    } catch (error) {
-      console.error(error);
+    if(this.form.valid) {
+      const auth = getAuth();
+      try {
+        const result = await signInWithEmailAndPassword(
+          auth,
+          this.form.value.email,
+          this.form.value.password
+        );
+        const idToken = await result.user.getIdToken();
+        console.log(idToken);
+        this._loginServices.login(idToken).subscribe((result: LoginResponse) => {
+          localStorage.setItem('token-recipes', result.token);
+        });
+        this._goToSearch();
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
 
   async registerWithEmailAndPassword() {
     const auth = getAuth();
     try {
-      const result = await createUserWithEmailAndPassword(auth, "christianrenovell83@gmail.com", "123456Abc");
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        this.form.value.email,
+        this.form.value.password
+      );
       const idToken = await result.user.getIdToken();
       console.log(idToken);
       this._loginServices.login(idToken).subscribe((result: LoginResponse) => {
-        localStorage.setItem("token-recipes", result.token);
+        localStorage.setItem('token-recipes', result.token);
       });
-      this._goToSearch()
+      this._goToSearch();
     } catch (error) {
       console.error(error);
     }
+  }
+
+  onChangeRegistryLogin() {
+    this.form.reset()
+    this.isRegistre.update(value => !value)
   }
 }
